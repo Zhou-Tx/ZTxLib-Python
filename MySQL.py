4d189d3af845332bc65bfde62567e4fac53f22b6
Zhou Tianxing <Zhou-Tx@users.noreply.github.com>
2019-09-19 18:09:08 +0800

    Update MySQL.py

--- MySQL.py
+++ MySQL.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
-from pymysql import connect
+from pymysql import connect, DatabaseError
 
 
 class MySQL:
     def __init__(self,
-                host='127.0.0.1',           # 要连接的主机地址
-                port=3306,                  # 端口
-                user='root',                # 用于登录的数据库用户
-                password=None,              # 密码
-                database=None,              # 要连接的数据库
-                passwd=None,                # 同 password，为了兼容 MySQLdb
-                db=None,                    # 同 database，为了兼容 MySQLdb
-                charset='utf8',             # 字符编码
-                connect_timeout=10          # 连接超时时间，(1-31536000)
-            ):
+                 host='127.0.0.1',          # 要连接的主机地址
+                 port=3306,                 # 端口
+                 user='root',               # 用于登录的数据库用户
+                 password=None,             # 密码
+                 database=None,             # 要连接的数据库
+                 passwd=None,               # 同 password，为了兼容 MySQLdb
+                 db=None,                   # 同 database，为了兼容 MySQLdb
+                 charset='utf8',            # 字符编码
+                 connect_timeout=10         # 连接超时时间，(1-31536000)
+                 ):
         self.__host = host
         self.__user = user
         self.__password = password
@@ -26,7 +26,7 @@ class MySQL:
         self.__passwd = passwd
 
     # Private, connect to the mysql database server
-    def __dbConnect(self):
+    def __db_connect(self):
         return connect(
             host=self.__host,
             user=self.__user,
@@ -39,64 +39,42 @@ class MySQL:
             passwd=self.__passwd
         )
 
-    def Fetchone(self, sql:str, args=None) -> 'Fetch the first record from the database with the sql sentence':
-        db = self.__dbConnect()
+    def Fetchone(self, sql: str, args=None) -> 'Fetch the first record from the database with the sql sentence':
+        db = self.__db_connect()
         cursor = db.cursor()
         try:
             cursor.execute(sql, args)
             return cursor.fetchone()
-        except:
+        except DatabaseError:
             db.rollback()
             return None
         finally:
             db.close()
 
-    def Fetchall(self, sql:str, args=None) -> 'Fetch all records from the database with the sql sentence':
-        db = self.__dbConnect()
+    def Fetchall(self, sql: str, args=None) -> 'Fetch all records from the database with the sql sentence':
+        db = self.__db_connect()
         cursor = db.cursor()
         try:
             cursor.execute(sql, args)
             return cursor.fetchall()
-        except:
+        except DatabaseError:
             db.rollback()
             return None
         finally:
             db.close()
 
-    def ExecuteNonQuery(self, sql:str, args=None) -> 'Execute a sql sentence to the database':
-        db = self.__dbConnect()
-        cursor = db.cursor()
-        try:
-            cursor.execute(sql, args)
-            db.commit()
-            return True
-        except Exception as e:
-            db.rollback()
+    def ExecuteNonQuery(self, sql: str, args=None) -> 'Execute sql sentences, args cannot be tuple if count > 1':
+        sql_list = sql.split(';')
+        if len(sql_list) > 1 and type(args) not in (type(None), dict):
             return False
-        finally:
-            db.close()
-
-    def ExecuteTrans(self, sql:list) -> 'Execute a transaction to the database with a list of sql sentence':
-        db = self.__dbConnect()
+        db = self.__db_connect()
         cursor = db.cursor()
         try:
-            for query_args in sql:
-                query = None
-                args = None
-                if type(query_args) == str:
-                    query = query_args
-                elif type(query_args) == tuple:
-                    query = query_args[0]
-                    if len(query_args) == 2:
-                        args = query_args[1]
-                    elif len(query_args) != 1:
-                        raise Exception()
-                else:
-                    raise Exception()
-                cursor.execute(query, args)
+            for sql in sql_list:
+                cursor.execute(sql, args)
             db.commit()
             return True
-        except Exception as e:
+        except DatabaseError:
             db.rollback()
             return False
         finally:
