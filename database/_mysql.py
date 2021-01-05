f0155dd20eeb73ff34dd7b4094be48005c4e38f0
Zhou Tianxing <z.t.x@foxmail.com>
2021-01-05 19:11:13 +0800

    Update requirements.txt
    Signed-off-by: Zhou Tianxing <z.t.x@foxmail.com>

--- database/_mysql.py
+++ database/_mysql.py
@@ -81,6 +81,7 @@ class MySQL:
             db.rollback()
             raise e
         finally:
+            cursor.close()
             db.close()
 
     def fetchall(self, sql: str, args=None) -> List[dict]:
@@ -108,6 +109,7 @@ class MySQL:
             db.rollback()
             raise e
         finally:
+            cursor.close()
             db.close()
 
     def update(self, sql: str, args=None) -> int:
@@ -138,4 +140,31 @@ class MySQL:
             db.rollback()
             raise e
         finally:
+            cursor.close()
             db.close()
+
+
+class MySQLConnectionPool:
+    def __init__(self,
+                 host='localhost',  # 要连接的主机地址
+                 port=3306,  # 端口
+                 user=None,  # 用于登录的数据库用户
+                 password=None,  # 密码
+                 database=None,  # 要连接的数据库
+                 passwd=None,  # 同 password，为了兼容 MySQLdb
+                 db=None,  # 同 database，为了兼容 MySQLdb
+                 charset='utf8mb4',  # 字符编码
+                 connect_timeout=5  # 连接超时时间，(1-31536000)
+                 ):
+        self.__host = host
+        self.__user = user
+        self.__password = password
+        self.__database = database
+        self.__port = port
+        self.__charset = charset
+        self.__connect_timeout = connect_timeout
+        self.__db = db
+        self.__passwd = passwd
+
+    def get_connection(self):
+        pass
